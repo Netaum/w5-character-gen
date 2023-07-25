32d956b4317d787baded47a8ee453691f98b489b
Antonio Neto <antonio.cechi@nubank.com.br>
2023-07-25 08:45:22 -0300

    Correction on tabs

--- src/modules/Navigation/navigation.js
+++ src/modules/Navigation/navigation.js
@@ -2,25 +2,31 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import './navigation.css';
 
-function Navigation () {
+function Navigation() {
     return (
-        <Tabs defaultActiveKey='sheet' 
-              id='navigation-tab-id'
-              className='navbar-custom'
-              justify>
+        <Tabs defaultActiveKey='sheet'
+            id='navigation-tab-id'
+            className='navbar-custom'
+            justify>
             <Tab eventKey="header" title="Header">
                 <div className='frame'>
-                    <h1>header</h1>
+                    <h1>NOP</h1>
                 </div>
             </Tab>
             <Tab eventKey="attributes" title="Attributes">
-                <h1>attributes</h1>
+                <div className='frame'>
+                    <h1>NOP</h1>
+                </div>
             </Tab>
             <Tab eventKey="skills" title="Skills">
-                <h1>skills</h1>
+                <div className='frame'>
+                    <h1>NOP</h1>
+                </div>
             </Tab>
             <Tab eventKey="powers" title="Powers">
-                <h1>powers</h1>
+                <div className='frame'>
+                    <h1>NOP</h1>
+                </div>
             </Tab>
         </Tabs>
     );
